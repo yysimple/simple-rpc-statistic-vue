2263df4c7248935c4ff779f6a510f340c76c127e
wuchengxing <1449697757@qq.com>
2022-06-23 00:01:35 +0800

    feat：初始化项目

--- src/main.js
+++ src/main.js
@@ -1,8 +1,28 @@
 import Vue from 'vue'
 import App from './App.vue'
+import 'ant-design-vue/dist/antd.css'
+import Antd from 'ant-design-vue'
+import router from './router'
+import Router from 'vue-router';
+import mavonEditor from 'mavon-editor'
+import 'mavon-editor/dist/css/index.css'
+import store from './store'
 
+
+// 全局组件注册
+import VCharts from 'v-charts'
+import components from './utils/components'
+
+// use
+Vue.use(mavonEditor)
+Vue.use(components)
+Vue.use(VCharts)
+Vue.use(Router)
 Vue.config.productionTip = false
+Vue.use(Antd)
 
 new Vue({
-  render: h => h(App),
+    router,
+    store,
+    render: h => h(App),
 }).$mount('#app')
