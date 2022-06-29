6e1de38d3fafe6ad64d288189eed628385c8c1d5
wuchengxing <1449697757@qq.com>
2022-06-29 11:02:13 +0800

    feat：trace树组件

--- src/views/trace/trace-view.vue
+++ src/views/trace/trace-view.vue
@@ -5,7 +5,21 @@
         placeholder="input search text"
         enter-button
         @search="onSearch"/>
-    <a-tree :tree-data="treeData" :replaceFields="replaceField" @select="onSelect"/>
+    <template>
+      <div id="components-a-tooltip-demo-placement">
+          <a-tooltip placement="right">
+            <template slot="title">
+              <span>prompt text</span>
+            </template>
+            <a-tree :tree-data="treeData" :replaceFields="replaceField" @select="onSelect"/>
+          </a-tooltip>
+      </div>
+<!--      <a-popover v-model="visible" title="Title" placement="right" trigger="click">-->
+<!--        <a slot="content" @click="hide"></a>-->
+<!--        <a-tree :tree-data="treeData" :replaceFields="replaceField" @select="onSelect"/>-->
+<!--      </a-popover>-->
+    </template>
+
   </div>
 </template>
 <script>
@@ -15,6 +29,8 @@ import {traceEntryTreeView, traceExitTreeView} from '@/api/trace'
 export default {
   data() {
     return {
+      visible: false,
+      buttonWidth: 70,
       treeData: [],
       replaceField: {children: 'children', title: 'showTitle', key: 'id'}
     };
@@ -32,10 +48,14 @@ export default {
     },
     onSearch(traceId) {
       console.log('', traceId)
-      this.traceEntryTreeVie(traceId)
+      this.traceEntryTreeVie('1541097580610588672')
     },
     onSelect(selectedNodes) {
       console.log('Trigger Select', selectedNodes[0]);
+    },
+    hide() {
+      console.log(111);
+      this.visible = false;
     },
   },
 };
@@ -44,5 +64,12 @@ export default {
 <style>
 .search-input {
   width: 400px;
+}
+#components-a-tooltip-demo-placement .ant-btn {
+  width: 70px;
+  text-align: center;
+  padding: 0;
+  margin-right: 8px;
+  margin-bottom: 8px;
 }
 </style>
